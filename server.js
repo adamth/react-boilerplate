7814bf1f764be854ea451f0a5c7a88b34b9e3a3f
Adam Thompson <adam@adamth.com>
2017-05-13 11:51:48 +1000

    Remove useless code

--- server.js
+++ server.js
@@ -11,17 +11,6 @@ app.use((req, res, next) => {
     }
 });
 
-if (process.env.NODE_ENV !== 'production') {
-    const webpackMiddleware = require('webpack-dev-middleware');
-    const webpack = require('webpack');
-    const webpackConfig = require('./webpack.config');
-
-    app.use(webpackMiddleware(webpack(webpackConfig)));
-} else {
-    app.use(express.static('dist'));
-}
-
-
 app.use(express.static('dist'));
 
 app.listen(PORT, () => {
